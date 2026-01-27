85d6b83850c7f8ad464f4f6e29fd0fe88a55d5b6
Fernanda Carles <fernanda.carles@gmail.com>
2026-01-27 10:52:40 -0300

    dbt: align project config and unpivot macro with silver architecture

--- dbt/macros/unpivot_measurements.sql
+++ dbt/macros/unpivot_measurements.sql
@@ -5,11 +5,28 @@
   {%- set raw_col = source_cfg.get("raw_payload_col", "raw_payload") -%}
   {%- set vars_map = source_cfg.get("variables", {}) -%}
 
+  {%- set cursor_col = source_cfg.get("cursor_id_col") -%}
+  {%- set is_valid_col = source_cfg.get("is_measured_at_valid_col") -%}
+
   {%- for variable_code, col_name in vars_map.items() %}
     select
       {{ station_col }} as station_code,
       '{{ source_name }}' as data_source_name,
-      {{ measured_col }} as measured_at,
+
+      {{ measured_col }} as measured_at_parsed,
+
+      {%- if cursor_col %}
+      {{ cursor_col }}::bigint as cursor_id,
+      {%- else %}
+      null::bigint as cursor_id,
+      {%- endif %}
+
+      {%- if is_valid_col %}
+      {{ is_valid_col }}::boolean as is_measured_at_valid,
+      {%- else %}
+      null::boolean as is_measured_at_valid,
+      {%- endif %}
+
       '{{ variable_code }}' as variable_code,
       {{ col_name }}::text as value_raw,
       {{ col_name }}::double precision as value_parsed,
