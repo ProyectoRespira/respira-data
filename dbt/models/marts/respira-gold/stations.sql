11533d3320c8d7b399e693914924a7456d1225da
Fernanda Carles <fernanda.carles@gmail.com>
2026-03-17 14:56:00 -0300

    respira-gold: station_id mapping from core to respira backend contract

--- dbt/models/marts/respira-gold/stations.sql
+++ dbt/models/marts/respira-gold/stations.sql
@@ -4,6 +4,10 @@ with stations as (
   select * from {{ ref('int_air_quality_stations') }}
 ),
 
+station_id_map as (
+  select * from {{ ref('int_station_id_map') }}
+),
+
 station_map as (
   select * from {{ ref('int_station_regions') }}
 ),
@@ -13,14 +17,20 @@ regions as (
 )
 
 select
-  s.id,
-  s.name,
+  sm.gold_station_id as id,
+  case
+    when lower(coalesce(s.properties->>'source', '')) = 'fiuna' then 'FIUNA: ' || s.name
+    when lower(coalesce(s.properties->>'source', '')) = 'airelibre' then 'AireLibre: ' || s.name
+    else s.name
+  end as name,
   s.latitude,
   s.longitude,
   r.id as region_id,
   (s.status = 'active') as is_station_on,
   coalesce(s.is_pattern_station, false) as is_pattern_station
 from stations s
+join station_id_map sm
+  on sm.core_station_id = s.id
 left join station_map m
   on m.station_code = s.code
 left join regions r
