863daa946ee0ef73212c4dabae3b76e6d9c7616c
Fernanda Carles <fernanda.carles@gmail.com>
2026-04-14 13:59:15 -0300

    set station.is_station_active to false when no new readings after 1 week

--- dbt/models/projects/respira_gold/gold/stations.sql
+++ dbt/models/projects/respira_gold/gold/stations.sql
@@ -12,6 +12,14 @@ station_map as (
   select * from {{ ref('int_station_regions') }}
 ),
 
+latest_station_readings as (
+  select
+    station_id,
+    max(date_localtime) as latest_reading_at
+  from {{ ref('station_readings_gold') }}
+  group by 1
+),
+
 regions as (
   select id, region_code from {{ ref('regions') }}
 )
@@ -26,12 +34,17 @@ select
   s.latitude,
   s.longitude,
   r.id as region_id,
-  (s.status = 'active') as is_station_on,
+  (
+    s.status = 'active'
+    and coalesce(lr.latest_reading_at >= now()::timestamptz - interval '7 days', false)
+  ) as is_station_on,
   coalesce(s.is_pattern_station, false) as is_pattern_station
 from stations s
 join station_id_map sm
   on sm.core_station_id = s.id
 left join station_map m
   on m.station_code = s.code
+left join latest_station_readings lr
+  on lr.station_id = sm.project_station_id
 left join regions r
   on r.region_code = m.region_code
